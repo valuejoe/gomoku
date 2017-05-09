04d9b1a6909a60787f8206251dd049ec6a7ac707
valuejoe <rockerjohnson01@gmail.com>
2017-05-09 14:56:46 +0800

    gomoku_MyJButton

---
+++ MyJButton.java
@@ -0,0 +1,11 @@
+package gomoku;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class MyJButton extends JButton {
+	public int pid=0;//½s¸¹0-3
+	public int r=0; //row
+	public int c=0; //column
+	
+}
